7e11b0583b76300ab3c876d38462319c09c1a397
Jyoti Sabane <jyotisabane7@gmail.com>
2026-08-18 13:50:03 +0530

    Time: 0 ms (100%), Space: 12.4 MB (54.61%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -6,15 +6,12 @@ class Solution(object):
         while ptr1 <= ptr2:
             if nums[ptr2] == val:
                 ptr2 -= 1
-
-            elif nums[ptr1] == val:
+            elif nums[ptr1] == val and nums[ptr2]!=val:
                 nums[ptr1], nums[ptr2] = nums[ptr2], nums[ptr1]
                 ptr1 += 1
                 ptr2 -= 1
-
             else:
                 ptr1 += 1
-
         return ptr2 + 1
         """
         :type nums: List[int]
