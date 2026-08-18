7092ec38f8c82deabc56d94bc51b782d11a42e2c
Jyoti Sabane <jyotisabane7@gmail.com>
2026-08-18 15:40:27 +0530

    Time: 18 ms (14.31%), Space: 14.5 MB (10.78%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        ele=nums[0]
+        count=0
+        for i in range(len(nums)):
+            if nums[i]==ele:
+                count+=1
+            else :
+                count-=1
+            if count==0:
+                ele=nums[i]
+                count+=1
+        return ele
+
+
+        
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
